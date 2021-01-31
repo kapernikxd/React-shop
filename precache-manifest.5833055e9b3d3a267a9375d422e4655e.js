24c1a43f0c96e7dea68087e3a6afb312bda02f23
kapernikxd <kapernikxd@gmail.com>
2021-01-31 21:39:33 +0300

    Updates

---
+++ precache-manifest.5833055e9b3d3a267a9375d422e4655e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c001ceb8cafac41cad75b01a4714745",
+    "url": "/React-shop/index.html"
+  },
+  {
+    "revision": "7a7ade3e508205f19bb3",
+    "url": "/React-shop/static/css/main.3bd0eb82.chunk.css"
+  },
+  {
+    "revision": "f1783f3fee16b8b5d4c5",
+    "url": "/React-shop/static/js/2.41756af1.chunk.js"
+  },
+  {
+    "revision": "7a7ade3e508205f19bb3",
+    "url": "/React-shop/static/js/main.d9ef0f25.chunk.js"
+  },
+  {
+    "revision": "d0db86ad2da416f45794",
+    "url": "/React-shop/static/js/runtime~main.0559d160.js"
+  }
+]);
